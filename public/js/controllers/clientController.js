0714f93d1de2c4cb8b27dee5356ccff063ddd326
arslan naseem <shani.8872842@gmail.com>
2015-09-19 21:02:48 +0500

    added list and album display

--- public/js/controllers/clientController.js
+++ public/js/controllers/clientController.js
@@ -8,8 +8,9 @@
                 $scope.title = "Clients";   //loads existing data from clients database
                         $http.post(path + '/load_clients').success(function (response) {
                                 $scope.clients = response;
+                                $scope.path = path;
                         });
-                    
+                    $scope.album = false; //true for album disply and false for list display
 
                 }]
 
